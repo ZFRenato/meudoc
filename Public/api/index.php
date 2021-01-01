4396f3b5550acffa8653be55d5aef4d0434ddb2b
Renato <renatopscarlos@gmail.com>
2021-01-01 18:17:10 -0300

    add gitignore

--- Public/api/index.php
+++ Public/api/index.php
@@ -7,7 +7,7 @@ require_once("../../vendor/autoload.php");
 header("Access-Controll-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Max-Age: 120");
+header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Header: Content-Type, Access-Control-Allow-Headers, Authorization, X-Request-With");
 
 // END HEADER
